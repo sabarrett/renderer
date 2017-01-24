28a4130515542ff55584485b916bc54dc8ddf8fd
Scott Barrett <scott.barrett@mymail.champlain.edu>
2017-01-23 22:01:47 -0500

    Added key identification code

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -g main.cpp -lX11
+	g++ -g -DXK_MISCELLANY -DXK_LATIN1 main.cpp -lX11
--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include <assert.h>
 #include <X11/Xlib.h>
 #include <unistd.h>
+#include <X11/keysymdef.h>
 
 int main(int argc, char* argv[])
 {
   Display* dsp;
   int blackColor, whiteColor;
+  Window wnd;
+  GC gc;
 
   dsp = XOpenDisplay(NULL);
   assert(dsp);
@@ -14,18 +17,14 @@ int main(int argc, char* argv[])
   blackColor = BlackPixel(dsp, DefaultScreen(dsp));
   whiteColor = WhitePixel(dsp, DefaultScreen(dsp));
 
-  Window wnd;
-
   wnd = XCreateSimpleWindow(dsp, DefaultRootWindow(dsp), 0, 0,
 			    200, 100, 0, blackColor, blackColor);
 
-  XSelectInput(dsp, wnd, StructureNotifyMask | ButtonPressMask | KeyPressMask);
-  // Calling XSelectInput again overrides previous input
-  //XSelectInput(dsp, wnd, ButtonPressMask);
+  XSelectInput(dsp, wnd, StructureNotifyMask | ButtonPressMask | KeyPressMask | KeyReleaseMask | PointerMotionMask);
 
   XMapWindow(dsp, wnd);
 
-  GC gc = XCreateGC(dsp, wnd, 0, (0));
+  gc = XCreateGC(dsp, wnd, 0, (0));
 
   XSetForeground(dsp, gc, whiteColor);
 
@@ -40,6 +39,16 @@ int main(int argc, char* argv[])
 	break;
       case KeyRelease:
 	printf("Key release\n");
+	{
+	  XKeyEvent b = e.xkey;
+	  int keysym = (int)XLookupKeysym(&b, 0); // Why 0? I'm not sure...
+	  printf(" Code %d\n", b.keycode);
+	  printf(" keysym %c\n", (int)XLookupKeysym(&b, 0));
+	  if (keysym == XK_Left)
+	    printf("***** LEFT ARROW PRESSED *****\n");
+	  if (keysym == XK_a)
+	    printf("** A PRESSED **\n");
+	}
 	break;
       case FocusIn:
 	printf("Focus in\n");
@@ -51,15 +60,19 @@ int main(int argc, char* argv[])
 	printf("Button Press\n");
 	break;
       case ButtonRelease:
-	printf("Button Release\n");
+	printf("Button Release");
 	break;
-      }
-    
-
-    if (e.type == MapNotify)
-      {
+      case MotionNotify:
+	printf("Mouse moved");
+	{
+	  XMotionEvent m = e.xmotion;
+	  //printf("{%d, %d}\n", m.x, m.y);
+	}
+	break;
+      case MapNotify:
 	XDrawLine(dsp, wnd, gc, 10, 60, 180, 20);
 	XFlush(dsp);
+	break;
       }
   }
 
